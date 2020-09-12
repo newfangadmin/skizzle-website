1a6ff0f9a21352f26ea811a69773a4e51c083f4e
myrnf <mayur@newfang.io>
2020-09-12 13:10:46 +0530

    updates for attempt===2

--- src/views/ContestHome.vue
+++ src/views/ContestHome.vue
@@ -4,7 +4,8 @@
     <div class="contestHome">
       <el-row>
         <el-col :span="22" :offset="1" class="sentContainer">
-          <div class="tryAgainSub"><span class="greeting">Hey {{this.firstName}},</span><br/>An email with a random keystore file has been sent to <span class="altColor">{{ this.email }}</span><br/><br/>You are currently on:</div>
+          <div class="sentSub" v-if="attempt===1"><span class="greeting">Hey {{this.firstName}},</span><br/>An email with a random keystore file has been sent to <span class="altColor">{{ this.email }}</span><br/><br/>You are currently on:</div>
+          <div class="sentSub" v-if="attempt===2"><span class="greeting">Hey {{this.firstName}},</span><br/>Send an email to 3 of your contacts with the attachment you downloaded.<br/>We will verify the email and send a 2nd random keystore to <span class="altColor">{{ this.email }}</span><br/><br/>You are currently on:</div>
           <el-card class="attemptCard">
             <div class="attemptHeading">Attempt {{ attempt }}</div>
           </el-card>
@@ -13,21 +14,31 @@
       <el-row class="whatNextContainer">
         <el-col class="whatNext" :xs="{span:24}" :sm="{span:20, offset:2}" :md="{span:20, offset:2}" :lg="{span:16, offset:4}">
           <div class="whatNextHeading">What next?</div>
-          <div class="whatNextSub">Your email from us should arrive shortly. Once it does, getting hold of the keystore file is fairly intuitive but here are some videos to help you out:</div>
+          <div class="whatNextSub" v-if="attempt===1">An email from us should arrive shortly. Once it does, getting hold of the keystore file is fairly intuitive but here are some videos to help you out:</div>
+          <div class="whatNextSub" v-if="attempt===2">Send the pdf file as a Skizzle encrypted attachment to 3 of your contacts and cc/bcc us in the same email. We will verify it and send over your 2nd random keystore.<br/>Sending a Skizzle encrypted attachment is fairly intuitive but here is a video to help you out:</div>
+          <ul class="whatNextList" v-if="attempt===2">
+            <li class="whatNextItem">Checkout how to add a Skizzle encrypted attachment and send it in <a href="https://youtube.com" target="_blank">this 14 second video</a>.</li>
+          </ul>
+          <div class="whatNextSub" v-if="attempt===2">Also check out:</div>
           <ul class="whatNextList">
             <li class="whatNextItem">Checkout how to install Skizzle in <a href="https://youtube.com" target="_blank">this 14 second video</a>.</li>
             <li class="whatNextItem">Checkout how to create a Skizzle account in <a href="https://youtube.com" target="_blank">this 12 second video</a>.</li>
             <li class="whatNextItem">Checkout how to decrypt and download the encrypted attachment in <a href="https://youtube.com" target="_blank">this 13 second video</a>.</li>
+            <li class="whatNextItem">Checkout how to import the keystore into your metamask in <a href="https://youtube.com" target="_blank">this 13 second video</a>.</li>
           </ul>
         </el-col>
       </el-row>
       <el-row class="tryAgainContainer">
         <el-col class="tryAgain" :xs="{span:24}" :sm="{span:20, offset:2}" :md="{span:20, offset:2}" :lg="{span:16, offset:4}">
           <div class="tryAgainHeading">Didn't win on your first attempt?</div>
-          <div class="tryAgainSub">Didn't win any tokens or just want to try and get our $100 grand prize? Have another go!</div>
+          <div class="tryAgainSub" v-if="attempt===1">Didn't win any tokens or just want to try and get our $100 grand prize? Have another go!</div>
+          <div class="tryAgainSub" v-if="attempt===2">Download the pdf below and send it as a Skizzle encrypted attachment to 3 of your contacts (also cc/bcc <span class="altColor">hello@skizzle.email</span>).</div>
         </el-col>
-        <el-col class="tryAgainBtnContainer" :span="24">
+        <el-col class="tryAgainBtnContainer" :span="24" v-if="attempt===1">
           <el-button icon="el-icon-magic-stick" @click="tryAgainInit">Try Again</el-button>
+        </el-col>
+        <el-col class="tryAgainBtnContainer" :span="24" v-if="attempt===2">
+          <el-button class="copyBtn" icon="el-icon-download" @click="downloadPDF">Download pdf</el-button>
         </el-col>
       </el-row>
     </div>
@@ -39,6 +50,16 @@
       <div class="sentDialogContainer">
         <div class="sentHeading">Your random keystore is on it's way to your inbox.</div>
         <img class="goodLuck" src="https://media.giphy.com/media/j1Xyt3DHfJcmk/giphy.gif" />
+      </div>
+    </el-dialog>
+    <el-dialog
+      title="Almost there!"
+      :visible.sync="mailSentDialogVisible2"
+      width="80%"
+      center>
+      <div class="sentDialogContainer">
+        <div class="sentHeading">We will verify your email and send you your second random keystore shortly after.</div>
+        <img class="goodLuck" src="https://media.giphy.com/media/12XDYvMJNcmLgQ/giphy.gif" />
       </div>
     </el-dialog>
 
@@ -72,6 +93,7 @@
 
 .sentContainer {
   text-align: center;
+  font-weight: 500;
 }
 
 .sentDialogContainer {
@@ -90,6 +112,10 @@
   margin-bottom: 30px;
   color: black;
   word-break: keep-all;
+}
+
+.whatNextSub, .tryAgainSub {
+  font-weight: 300;
 }
 
 .attemptCard {
@@ -224,7 +250,8 @@ export default {
     return {
       attempt: 1,
       token: "",
-      mailSentDialogVisible: true,
+      mailSentDialogVisible: false,
+      mailSentDialogVisible2: false,
       twitterDialogVisible: false,
       tweetText: "I'm going for gold with the @skizzledotemail contest! Check it out https://skizzle.email/contest It is the most secure way to share files.",
       tweetLink: "",
@@ -253,6 +280,23 @@ export default {
       document.execCommand('copy');
     },
 
+    async secondAttempt() {
+      const res = await fetch("https://contest-server.skizzle.email/wallet/new-attempt", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Token ${this.token}`
+        }
+      });
+      if (res.status < 400) {
+        res.json().then((result) => {
+          console.log(result);
+          this.mailSentDialogVisible2 = true;
+          this.attempt = 2;
+        }); 
+      }
+    },
+
     downloadPDF() {
       fetch(this.url, {
         method: 'get',
@@ -265,7 +309,8 @@ export default {
           a.download = "some.png";
           document.body.appendChild(a); // we need to append the element to the dom -> otherwise it will not work in firefox
           a.click();    
-          a.remove();  //afterwards we remove the element again         
+          a.remove();  //afterwards we remove the element again
+          this.secondAttempt();
       })
       .catch(() => console.log('error occured'));
     }
@@ -274,7 +319,7 @@ export default {
     this.$root.$emit('gotEmail', this.email);
     this.token = localStorage.getItem(`skizzleContest|${this.email}`);
     if (!this.token) {
-      // this.$router.push("/contest");
+      this.$router.push("/contest");
     }
     if (JSON.parse(this.firstLogin)) {
       this.mailSentDialogVisible = true;
